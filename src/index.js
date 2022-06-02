035a09f076ada268c7493c563796021f4fcd2170
Mathew Francisco <mhfrancisco775@gmail.com>
2022-06-03 05:17:22 +0800

    Continued with webpack configuration

---
+++ src/index.js
@@ -0,0 +1,23 @@
+window.CMS_MANUAL_INIT = true
+
+import CMS, { init } from 'netlify-cms-app'
+
+init({
+  config: {
+    backend: {
+      name: 'test-repo',
+    },
+    
+    load_config_file: false,
+    media_folder: "static/images/uploads",
+    public_folder: "/images/uploads",
+    collections: [
+      { label: "Blog", name: "blog", folder: "_posts/blog", create: true, fields: [
+        { label: "Title", name: "title", widget: "string" },
+        { label: "Publish Date", name: "date", widget: "datetime" },
+        { label: "Featured Image", name: "thumbnail", widget: "image" },
+        { label: "Body", name: "body", widget: "markdown" },
+      ]},
+    ],
+  },
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,31 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src/admin/index.js'),
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Storefront Netlify CMS',
+      filename: 'index.html'
+    })
+  ],
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'dist')
